d0f96c6e98948a623a89f5a4d68cc560d5899c1a
Dara Olayebi <dara.olayebi@gmail.com>
2021-03-15 01:50:56 +0100

    form fixed by adding ids to input

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
 		<React.Fragment>
 			<section className="intro">
 				<div className="container">
-					<h1 className="title is-1">Find creative talent across Africa</h1>
+					<h1 className="title">Find creative talent across Africa</h1>
 					<p className="subtitle">
 						Be a part of our creator's community or find designers who are looking for their next opportunity.
 					</p>
